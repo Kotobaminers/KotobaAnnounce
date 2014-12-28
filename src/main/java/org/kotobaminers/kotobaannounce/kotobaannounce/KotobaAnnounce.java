8d9e24ec68e5ab22d976f0e56128b438355e6262
Thasan <thasan@koodaa.net>
2014-12-29 01:03:32 +0200

    Starting making scheduler

--- src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
+++ src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
@@ -7,6 +7,15 @@ public class KotobaAnnounce extends JavaPlugin {
 	public void onEnable() {
 		System.out.println("Enabling KotobaAnnounce");
 		saveConfig();
+		
+        getServer().getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run() {
+                // Do something
+            	Scheduler.run();
+            }
+        }, 200L, 20L); // Waiting 10sec before first run, then run every sec
+        
 	}
 
 	@Override
---
+++ src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/Scheduler.java
@@ -0,0 +1,11 @@
+package org.kotobaminers.kotobaannounce.kotobaannounce;
+
+public final class Scheduler {
+
+	public static void run() {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	
+}
