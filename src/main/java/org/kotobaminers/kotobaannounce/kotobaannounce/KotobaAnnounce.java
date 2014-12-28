f9d5527939c75f1534e5cb460af3e6991ecc8966
kai_f <fukushima.kai@gmail.com>
2014-12-29 04:26:54 +0900

    Added plugin.yml and config.yml

--- src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
+++ src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
@@ -1,5 +1,17 @@
 package org.kotobaminers.kotobaannounce.kotobaannounce;
 
-public class KotobaAnnounce {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class KotobaAnnounce extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		System.out.println("Enabling KotobaAnnounce");
+		saveConfig();
+	}
+
+	@Override
+	public void onDisable() {
+		System.out.println("Disabling KotobaAnnounce");
+		saveConfig();
+	}
 }
