07258a61e953d6834aaaf829ac1f924214ab9177
kai_f <fukushima.kai@gmail.com>
2014-12-30 04:11:31 +0900

    still in the way

--- src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
+++ src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/KotobaAnnounce.java
@@ -1,11 +1,19 @@
 package org.kotobaminers.kotobaannounce.kotobaannounce;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.kotobaminers.kotobaannounce.kotobaannounce.CommandExecutorKotobaAnnounce.Commands;
 
 public class KotobaAnnounce extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		System.out.println("Enabling KotobaAnnounce");
+
+		StaticFields.initialize(this);
+
+		for(Commands command : Commands.values()) {
+			this.getCommand(command.toString()).setExecutor(new CommandExecutorKotobaAnnounce(this));
+		}
+
 		saveConfig();
 	}
 
