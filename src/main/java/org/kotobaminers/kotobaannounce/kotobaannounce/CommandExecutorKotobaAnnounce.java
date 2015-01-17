9d6a360e5bce1e8d8a05e4b02f7c4a38edf241d1
Thasan <thasan@koodaa.net>
2015-01-17 12:57:04 +0200

    Merging CommandKotobaAnnounce.java to CommandExecutorKotobaAnnouce.java

--- src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/CommandExecutorKotobaAnnounce.java
+++ src/main/java/org/kotobaminers/kotobaannounce/kotobaannounce/CommandExecutorKotobaAnnounce.java
@@ -15,28 +15,91 @@ public class CommandExecutorKotobaAnnounce implements CommandExecutor {
 			}
 		}
 	}
+	
+
+	
 	public CommandExecutorKotobaAnnounce(KotobaAnnounce plugin) {
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label,String[] args) {
 		if(sender instanceof Player) {
 			Player player = (Player) sender;
-			if(player.isOp()) {
-				Commands commands = Commands.lookup(label);
-				switch(commands) {
-				case ANNOUNCE:
-					new CommandKotobaAnnounce().runCommand(player, command, args);
+			if(player.isOp() == false) return false;	// Failing if not OP
+		}
+		
+		Player player = null;
+		Commands commands = Commands.lookup(label);
+		switch(commands) {
+		case ANNOUNCE:
+			runKotobaAnnounceCommand(player, command, args);
+			return true;
+		case KOTOBAANNOUNCE:
+			runKotobaAnnounceCommand(player, command, args);
+			return true;
+		case NONE:
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+	
+	
+	public enum KotobaAnnounceCommands {NONE, RELOAD, INTERVAL, TOGGLE, DEBUG;
+		public static KotobaAnnounceCommands lookup(String name) {
+			try {
+				return KotobaAnnounceCommands.valueOf(name.toUpperCase());
+			} catch (IllegalArgumentException e) {
+				return KotobaAnnounceCommands.NONE;
+			}
+		}
+	}
+	public boolean runKotobaAnnounceCommand(Player player, Command command, String[] args) {
+		Utilities.prindDebug("", new Exception());
+		if(0 < args.length) {
+			KotobaAnnounceCommands commands = KotobaAnnounceCommands.lookup(args[0]);
+			switch(commands) {
+			case INTERVAL:
+				if(1 < args.length) {
+					commandInterval(args[1]);
 					return true;
-				case KOTOBAANNOUNCE:
-					new CommandKotobaAnnounce().runCommand(player, command, args);
-					return true;
-				case NONE:
-					break;
-				default:
-					break;
 				}
+			case RELOAD:
+				commandReload();
+				return true;
+			case TOGGLE:
+				commandToggle(player);
+				return true;
+			case DEBUG:
+				Utilities.toggleDebug();
+				return true;
+			case NONE:
+				break;
+			default:
+				break;
 			}
 		}
 		return false;
 	}
+	private void commandToggle(Player player) {
+		Utilities.prindDebug("", new Exception());
+		Config.reverseToggle();
+		Config.printToggle(player);
+	}
+	private void commandReload() {
+		Utilities.prindDebug("", new Exception());
+		StaticFields.initialize(StaticFields.plugin);
+	}
+	private void commandInterval(String arg) {
+		Utilities.prindDebug("", new Exception());
+		Integer interval = 100;
+		try{
+			interval = Integer.valueOf(arg);
+		} catch(NumberFormatException e) {
+			Utilities.prindDebug(e.toString(), new Exception());
+			return;
+		}
+		Config.saveInterval(interval);
+	}
+	
 }
